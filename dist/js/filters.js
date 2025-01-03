b3e63b27481039041dc8081137f5f90564c30473
Anindia Agusta <141204784+anindiagusta@users.noreply.github.com>
2025-01-03 15:37:40 +0700

    Add files via upload

--- dist/js/filters.js
+++ dist/js/filters.js
@@ -1,1 +1,30 @@
+const dropdowns = document.querySelectorAll('.dropdown');
 
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.select');
+  const caret = dropdown.querySelector('.caret');
+  const menu2 = dropdown.querySelector('.menu2');
+  const options = dropdown.querySelectorAll('.menu2 li');
+  const selected = dropdown.querySelector('.selected');
+
+
+
+  select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    menu2.classList.toggle('menu2-open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      selected.innerText = option.innerText;
+      select.classList.remove('select-clicked');
+      caret.classList.remove('caret-rotate');
+      menu2.classList.remove('menu2-open');
+      options.forEach(option => {
+        option.classList.remove('active');
+      });
+      option.classList.add('active')
+    });
+  });
+});
